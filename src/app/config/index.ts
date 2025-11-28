2a362ec69435411fb8a9e34af9c542281da00dec
marcsitze <marcsitze01@gmail.com>
2025-11-28 16:25:21 +0100

    fix: change api url

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,3 +1,3 @@
 // export const API_URL = "http://localhost:8000/api/v1"
 // export const API_URL = "http://192.168.1.169:8000/api/v1"
-export const API_URL = "https://suspicious-alice-warap-dbf36317.koyeb.app/api/v1"
+export const API_URL = "https://confidential-zebra-warap-7cbbaf0d.koyeb.app/"
